236d400224a4c1f6dabbc54498db3f4031b88778
Md. Sayeed Hassan Maheen <sayeedhassan1124@gmail.com>
2025-03-10 08:38:01 +0600

    All Messages and Create message API implemented

--- lib/screens/user/user_group_chat_screen/controller/user_group_chat_controller.dart
+++ lib/screens/user/user_group_chat_screen/controller/user_group_chat_controller.dart
@@ -1,33 +1,51 @@
-// lib/screens/user/user_group_chat_screen/controller/user_group_chat_controller.dart
 import 'package:get/get.dart';
 
 import '../../../../models/my_group_model.dart';
+import '../../../../services/repository/my_group_repository/my_group_repository.dart';
 
 class UserGroupChatController extends GetxController {
   final MyGroup group;
   RxList<MyGroup> messages = <MyGroup>[].obs;
   final RxString receiverName = ''.obs;
   RxBool isLoading = false.obs;
+  final MyGroupRepository _myGroupRepository = MyGroupRepository();
 
   UserGroupChatController({required this.group}) {
     receiverName.value = group.name ?? 'Unknown Group';
-    initializeMessages();
+    fetchMessages();
   }
 
-  void initializeMessages() {
-    if (group.message != null && group.message!.isNotEmpty) {
-      messages.add(group);
+  void fetchMessages() async {
+    try {
+      isLoading.value = true;
+      final fetchedMessages =
+          await _myGroupRepository.fetchMessagesByGroupId(group.id!);
+      if (fetchedMessages != null) {
+        messages.value = fetchedMessages;
+      }
+    } catch (e) {
+      print('Error fetching messages: $e');
+    } finally {
+      isLoading.value = false;
     }
   }
 
-  void sendMessage(String content) {
+  void sendMessage(String content) async {
     if (content.isNotEmpty) {
-      messages.add(MyGroup(
-        id: DateTime.now().millisecondsSinceEpoch.toString(),
-        message: content,
-        from: group.from,
-        createdAt: DateTime.now(),
-      ));
+      try {
+        final success =
+            await _myGroupRepository.postMessage(group.id!, content);
+        if (success) {
+          messages.add(MyGroup(
+            id: DateTime.now().millisecondsSinceEpoch.toString(),
+            message: content,
+            from: group.from,
+            createdAt: DateTime.now(),
+          ));
+        }
+      } catch (e) {
+        print('Error sending message: $e');
+      }
     }
   }
 }
--- lib/screens/user/user_group_chat_screen/user_group_chat_screen.dart
+++ lib/screens/user/user_group_chat_screen/user_group_chat_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:itzel/models/my_group_model.dart';
 
 import '../../../constants/app_colors.dart';
 import '../../../constants/app_icons_path.dart';
+import '../../../models/my_group_model.dart';
 import '../../../widgets/icon_widget/icon_widget.dart';
 import '../../../widgets/text_widget/text_widgets.dart';
 import 'controller/user_group_chat_controller.dart';
@@ -28,8 +28,6 @@ class _UserGroupChatScreenState extends State<UserGroupChatScreen> {
     controller = Get.put(UserGroupChatController(group: widget.group!));
   }
 
-  // Move messages list to state
-
   @override
   void dispose() {
     messageController.dispose();
@@ -45,140 +43,78 @@ class _UserGroupChatScreenState extends State<UserGroupChatScreen> {
       ),
       child: Scaffold(
         backgroundColor: AppColors.whiteBg,
-        // appBar: UserGroupAppBarWidget(
-        //   onTitleTap: () {
-        //     Get.to(() => UserGroupChatInfoScreen());
-        //   },
-        //   onInfoTap: () {
-        //     Get.to(() => UserGroupChatInfoScreen());
-        //   },
-        //   onMoreTap: () {
-        //     showMenu(
-        //       context: context,
-        //       position: const RelativeRect.fromLTRB(
-        //           100, 100, 0, 0), // Adjust the position as needed
-        //       items: [
-        //         const PopupMenuItem<int>(
-        //           value: 0,
-        //           child: Row(
-        //             children: [
-        //               Icon(Icons.delete_outline,
-        //                   color: AppColors.black500, size: 20),
-        //               SpaceWidget(spaceWidth: 6),
-        //               Text('Delete Chat'),
-        //             ],
-        //           ),
-        //         ),
-        //         const PopupMenuItem<int>(
-        //           value: 1,
-        //           child: Row(
-        //             children: [
-        //               Icon(Icons.block, color: AppColors.black500, size: 20),
-        //               SpaceWidget(spaceWidth: 6),
-        //               Text('Block'),
-        //             ],
-        //           ),
-        //         ),
-        //         const PopupMenuItem<int>(
-        //           value: 1,
-        //           child: Row(
-        //             children: [
-        //               Icon(Icons.report, color: AppColors.black500, size: 20),
-        //               SpaceWidget(spaceWidth: 6),
-        //               Text('Report Problem'),
-        //             ],
-        //           ),
-        //         ),
-        //         // Add more PopupMenuItem widgets for additional menu items
-        //       ],
-        //     ).then((value) {
-        //       if (value != null) {
-        //         // Handle menu item selection
-        //         switch (value) {
-        //           case 0:
-        //             // Action for first menu item
-        //             break;
-        //           case 1:
-        //             // Action for second menu item
-        //             break;
-        //           // Add more cases for additional menu items
-        //           case 2:
-        //             // Action for second menu item
-        //             break;
-        //           // Add more cases for additional menu items
-        //         }
-        //       }
-        //     });
-        //   },
-        //   titleText: controller.receiverName.value,
-        //   subTitleText: 'Last Seen 12h ago',
-        // ),
         appBar: AppBar(
           title: Obx(() => Text(controller.receiverName.value)),
         ),
         body: Stack(
           children: [
-            // Chat messages list
-            ListView.builder(
-              itemCount: controller.messages.length,
-              padding: const EdgeInsets.only(top: 10, bottom: 70),
-              itemBuilder: (context, index) {
-                final message = controller.messages[index];
-                final isReceived =
-                    message.from?.id != controller.group.from?.id;
+            Obx(() {
+              if (controller.isLoading.value) {
+                return const Center(child: CircularProgressIndicator());
+              } else {
+                return ListView.builder(
+                  itemCount: controller.messages.length,
+                  padding: const EdgeInsets.only(top: 10, bottom: 70),
+                  itemBuilder: (context, index) {
+                    final message = controller.messages[index];
+                    final isReceived =
+                        message.from?.id != controller.group.from?.id;
 
-                return Container(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: size.width / (size.width / 14),
-                    vertical: size.width / (size.width / 10),
-                  ),
-                  child: Align(
-                    alignment:
-                        isReceived ? Alignment.topLeft : Alignment.topRight,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color:
-                            isReceived ? AppColors.white : AppColors.blueNormal,
-                        boxShadow: const [
-                          BoxShadow(
-                            color: AppColors.grey300,
-                            spreadRadius: 1,
-                            blurRadius: 3,
-                          ),
-                        ],
+                    return Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: size.width / (size.width / 14),
+                        vertical: size.width / (size.width / 10),
                       ),
-                      padding: EdgeInsets.all(size.width / (size.width / 16)),
-                      child: Column(
-                        crossAxisAlignment: isReceived
-                            ? CrossAxisAlignment.start
-                            : CrossAxisAlignment.end,
-                        children: [
-                          if (message.from?.name != null)
-                            TextWidget(
-                              text: message.from!.name!,
-                              fontColor: isReceived
-                                  ? AppColors.grey600
-                                  : AppColors.white,
-                              fontSize: 11,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          TextWidget(
-                            text: message.message ?? '',
-                            fontColor: isReceived
-                                ? AppColors.black500
-                                : AppColors.white,
-                            fontSize: 13,
-                            fontWeight: FontWeight.w400,
+                      child: Align(
+                        alignment:
+                            isReceived ? Alignment.topLeft : Alignment.topRight,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: isReceived
+                                ? AppColors.white
+                                : AppColors.blueNormal,
+                            boxShadow: const [
+                              BoxShadow(
+                                color: AppColors.grey300,
+                                spreadRadius: 1,
+                                blurRadius: 3,
+                              ),
+                            ],
                           ),
-                        ],
+                          padding:
+                              EdgeInsets.all(size.width / (size.width / 16)),
+                          child: Column(
+                            crossAxisAlignment: isReceived
+                                ? CrossAxisAlignment.start
+                                : CrossAxisAlignment.end,
+                            children: [
+                              if (message.from?.name != null)
+                                TextWidget(
+                                  text: message.from!.name!,
+                                  fontColor: isReceived
+                                      ? AppColors.grey600
+                                      : AppColors.white,
+                                  fontSize: 11,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              TextWidget(
+                                text: message.message ?? '',
+                                fontColor: isReceived
+                                    ? AppColors.black500
+                                    : AppColors.white,
+                                fontSize: 13,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
+                    );
+                  },
                 );
-              },
-            ),
-            // Bottom input field
+              }
+            }),
             Align(
               alignment: Alignment.bottomCenter,
               child: Padding(
@@ -221,11 +157,10 @@ class _UserGroupChatScreenState extends State<UserGroupChatScreen> {
                         ),
                       ),
                     ),
-                    // Send button
                     GestureDetector(
                       onTap: () {
-                        // sendMessage(messageController.text);
-                        //  messageController.clear();
+                        controller.sendMessage(messageController.text);
+                        messageController.clear();
                       },
                       child: Container(
                         height: size.width / (size.width / 48),
